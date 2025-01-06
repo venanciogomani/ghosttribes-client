8101b19eeba1b3ec947fadba339293f4da927d7d
venanciogomani <venanciogomani@gmail.com>
2025-01-06 18:12:08 +0100

    product card hover state

--- src/components/shared/Card/Card.tsx
+++ src/components/shared/Card/Card.tsx
@@ -1,4 +1,5 @@
 import { FavoriteOutlined, ShoppingCartOutlined, VisibilityOutlined } from "@mui/icons-material";
+import { useState } from "react";
 
 interface ICard {
     title: string;
@@ -9,8 +10,14 @@ interface ICard {
 }
 
 export default function Card({ title, img, price, oldPrice = 0, isNew }: ICard) {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-        <div className="product w-1/4 flex flex-col p-2 border-2 cursor-pointer border-slate-900/0 hover:border-slate-900 transition ease-in-out duration-300 relative">
+        <div 
+            className="product w-1/4 flex flex-col p-2 border-2 cursor-pointer border-slate-900/0 hover:border-slate-900 transition ease-in-out duration-300 relative"
+            onMouseOver={() => setIsHovered(true)}
+            onMouseOut={() => setIsHovered(false)}
+        >
             <div className="w-full h-80">
                 <img src={img} alt={title} className="object-cover w-full h-full flex-shrink-0" />
             </div>
@@ -22,17 +29,19 @@ export default function Card({ title, img, price, oldPrice = 0, isNew }: ICard) 
                 <h2 className="pt-2 font-semibold text-slate-800 hover:text-pink-600 transition ease-in-out duration-300">{title}</h2>
             </div>
             {isNew && <div className="new-product absolute top-2 left-2 bg-pink-600 text-white text-xs font-semibold p-1">New</div>}
-            <div className="absolute top-2 right-2 flex flex-col gap-y-2 p-2">
-                <div className="bg-slate-800 hover:bg-pink-600 p-1 rounded-full transition ease-in-out duration-300 flex items-center justify-center">
-                    <ShoppingCartOutlined className="text-white" fontSize="small" />
+            {isHovered && 
+                <div className="absolute top-2 right-2 flex flex-col gap-y-2 p-2">
+                    <div className="bg-slate-800 hover:bg-pink-600 p-1 rounded-full transition ease-in-out duration-300 flex items-center justify-center">
+                        <ShoppingCartOutlined className="text-white" fontSize="small" />
+                    </div>
+                    <div className="bg-slate-800 hover:bg-pink-600 p-1 rounded-full transition ease-in-out duration-300 flex items-center justify-center">
+                        <FavoriteOutlined className="text-white" fontSize="small" />
+                    </div>
+                    <div className="bg-slate-800 hover:bg-pink-600 p-1 rounded-full transition ease-in-out duration-300 flex items-center justify-center">
+                        <VisibilityOutlined className="text-white" fontSize="small" />
+                    </div>
                 </div>
-                <div className="bg-slate-800 hover:bg-pink-600 p-1 rounded-full transition ease-in-out duration-300 flex items-center justify-center">
-                    <FavoriteOutlined className="text-white" fontSize="small" />
-                </div>
-                <div className="bg-slate-800 hover:bg-pink-600 p-1 rounded-full transition ease-in-out duration-300 flex items-center justify-center">
-                    <VisibilityOutlined className="text-white" fontSize="small" />
-                </div>
-            </div>
+            }
         </div>
     )
 }
