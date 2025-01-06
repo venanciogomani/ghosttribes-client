b0888d40fd6eb36e07799eb594b6d32d53b9a6e0
venanciogomani <venanciogomani@gmail.com>
2025-01-06 18:04:30 +0100

    product card ui

--- src/components/shared/Card/Card.tsx
+++ src/components/shared/Card/Card.tsx
@@ -1,3 +1,5 @@
+import { FavoriteOutlined, ShoppingCartOutlined, VisibilityOutlined } from "@mui/icons-material";
+
 interface ICard {
     title: string;
     img: string;
@@ -13,16 +15,24 @@ export default function Card({ title, img, price, oldPrice = 0, isNew }: ICard) 
                 <img src={img} alt={title} className="object-cover w-full h-full flex-shrink-0" />
             </div>
             <div className="py-2">
-                <div className="flex justify-between w-full">
-                    <div className="flex items-center justify-between w-1/2 border-2 border-pink-600 py-1 px-2">
-                        {oldPrice > 0 && <p className="text-sm text-slate-600 line-through">${oldPrice}</p>}
-                        <p className="text-sm font-semibold text-slate-700">${price}</p>
-                    </div>
-                    <button className="btn w-1/2 bg-slate-800 text-sm text-white font-semibold">Add to Cart</button>
+                <div className="flex items-center justify-center w-full border-2 border-pink-600 py-1 px-2 gap-x-2 lg:gap-x-4">
+                    {oldPrice > 0 && <p className="text-sm text-slate-600 line-through">${oldPrice}</p>}
+                    <p className="text-sm font-semibold text-slate-700">${price}</p>
                 </div>
-                <h2 className="pt-2 font-semibold">{title}</h2>
+                <h2 className="pt-2 font-semibold text-slate-800 hover:text-pink-600 transition ease-in-out duration-300">{title}</h2>
             </div>
             {isNew && <div className="new-product absolute top-2 left-2 bg-pink-600 text-white text-xs font-semibold p-1">New</div>}
+            <div className="absolute top-2 right-2 flex flex-col gap-y-2 p-2">
+                <div className="bg-slate-800 hover:bg-pink-600 p-1 rounded-full transition ease-in-out duration-300 flex items-center justify-center">
+                    <ShoppingCartOutlined className="text-white" fontSize="small" />
+                </div>
+                <div className="bg-slate-800 hover:bg-pink-600 p-1 rounded-full transition ease-in-out duration-300 flex items-center justify-center">
+                    <FavoriteOutlined className="text-white" fontSize="small" />
+                </div>
+                <div className="bg-slate-800 hover:bg-pink-600 p-1 rounded-full transition ease-in-out duration-300 flex items-center justify-center">
+                    <VisibilityOutlined className="text-white" fontSize="small" />
+                </div>
+            </div>
         </div>
     )
 }
